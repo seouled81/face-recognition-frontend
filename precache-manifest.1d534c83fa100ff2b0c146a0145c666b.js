c72efdc828a30871b181e800577eade4f597158e
MacBook di Enrico <Enrico@MacBook-di-MacBook.local>
2019-01-30 11:54:08 +0900

    Updates

---
+++ precache-manifest.1d534c83fa100ff2b0c146a0145c666b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8db2c86932eedb1fcf98",
+    "url": "/face-recognition-frontend/static/js/runtime~main.8db2c869.js"
+  },
+  {
+    "revision": "ce8c17ea6a697b2c4896",
+    "url": "/face-recognition-frontend/static/js/main.ce8c17ea.chunk.js"
+  },
+  {
+    "revision": "53bc238cde67b948d292",
+    "url": "/face-recognition-frontend/static/js/1.53bc238c.chunk.js"
+  },
+  {
+    "revision": "ce8c17ea6a697b2c4896",
+    "url": "/face-recognition-frontend/static/css/main.48ef0763.chunk.css"
+  },
+  {
+    "revision": "53bc238cde67b948d292",
+    "url": "/face-recognition-frontend/static/css/1.59104a19.chunk.css"
+  },
+  {
+    "revision": "43680835576100825c88699ee7828aaa",
+    "url": "/face-recognition-frontend/index.html"
+  }
+];
